4eda9b664e9141895286b02f11759eb94e722e1b
Doa <doadoadaodao5@gmail.com>
2021-11-28 10:57:21 +0100

    Content of files

---
+++ CRUD_Operation/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Operation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+
+        }
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-H5OIFL6H\\SQLEXPRESS;Initial Catalog=CRUD_SP_DB;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string status = "";
+            if (radioButton1.Checked == true)
+            {
+                status = radioButton1.Text;
+            }
+            else
+            {
+                status = radioButton2.Text;
+            }
+            SqlCommand com = new SqlCommand("exec dbo.SP_Product_Insert '" + int.Parse(textBox1.Text) + "','" + textBox2.Text + "','" + comboBox1.Text + "','" + status + "','" + DateTime.Parse(dateTimePicker1.Text) + "'", con);
+            com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Saved");
+            LoadAllRecords();
+        }
+
+        void LoadAllRecords()
+        {
+            SqlCommand com = new SqlCommand("exec dbo.SP_Product_View", con);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadAllRecords(); 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string status = "";
+            if (radioButton1.Checked == true)
+            {
+                status = radioButton1.Text;
+            }
+            else
+            {
+                status = radioButton2.Text;
+            }
+            SqlCommand com = new SqlCommand("exec dbo.SP_Product_Update '" + int.Parse(textBox1.Text) + "','" + textBox2.Text + "','" + comboBox1.Text + "','" + status + "','" + DateTime.Parse(dateTimePicker1.Text) + "'", con);
+            com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Updated");
+            LoadAllRecords();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (MessageBox.Show("Are you sure you to delete?","Delete",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("exec dbo.SP_Product_Delete '" + int.Parse(textBox1.Text) + "'", con);
+                com.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Successfully Deleted");
+                LoadAllRecords();
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand com = new SqlCommand("exec dbo.SP_Product_search'"+int.Parse(textBox1.Text)+"'", con);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+    
+}
